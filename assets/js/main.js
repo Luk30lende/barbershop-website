fb1eeff201061e9966380ff36a02854194639685
Luk30lende <olendeluke2k@gmail.com>
2026-09-13 20:51:33 +0300

    Header and Navigation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,49 +1,57 @@
 /*=============== SHOW & CLOSE MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
+//Show Menu
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+// Hide Menu
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
 /*=============== REMOVE MOBILE MENU ===============*/
+const navLink = document.querySelectorAll(".nav__link");
 
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== CHANGE HEADER STYLES ===============*/
 
-
 /*=============== SWIPER WORKS ===============*/
-
 
 /*=============== SWIPER TESTIMONIAL ===============*/
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
-/*=============== GSAP ANIMATION ===============*/ 
-
+/*=============== GSAP ANIMATION ===============*/
 
 /* Home animation */
 
-
 /* Home background animation */
-
 
 /* The nav animation only works in the home section */
 
-
 /* About animation */
-
 
 /* Work animation */
 
-
 /* Service animation */
-
 
 /* Expert animation */
 
-
 /* Contact animation */
-
 
 /* Footer animation */
