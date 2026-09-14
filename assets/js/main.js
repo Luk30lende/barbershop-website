f302098417b338cecf410cdec4387548251dbfd5
Luk30lende <olendeluke2k@gmail.com>
2026-09-14 17:49:41 +0300

    Service section

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,6 +54,21 @@ const swiperWork = new Swiper(".work__swiper", {
 });
 
 /*=============== SWIPER TESTIMONIAL ===============*/
+const swiperTestimonial = new Swiper(".service__swiper", {
+  loop: true,
+  slidesPerView: "auto",
+  spaceBetween: 56,
+  grabCursor: true,
+
+  pagination: {
+    el: ".service__swiper .swiper-pagination",
+  },
+
+  navigation: {
+    nextEl: ".service__swiper .swiper-button-next",
+    prevEl: ".service__swiper .swiper-button-prev",
+  },
+});
 
 /*=============== SHOW SCROLL UP ===============*/
 
